c63de7d96212d2572ca574c77aa604f57fb67e6d
Madhura Gundgal <madhuragundgal45@gmail.com>
2026-02-01 02:12:00 +0530

    Streamlit App

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+# ---------- Recommendation Function ----------
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movies_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
+
+
+# ---------- Load Data ----------
+movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movie_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
+# ---------- Streamlit UI ----------
+st.title('🎬 Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    'Select a movie:',
+    movies['title'].values
+)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for movie in recommendations:
+        st.write(movie)
